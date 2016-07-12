8a4d50fbb8e4e3d317eaf748ccc431203e8dd3da
Kochin Wang <kcwang.work@gmail.com>
2016-07-12 19:01:45 +0800

    新增測試案例且執行後亮紅燈

---
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+        [TestMethod]
+        public void Test_Assert_failed()
+        {
+            Assert.Fail();
+        }
+    }
+}

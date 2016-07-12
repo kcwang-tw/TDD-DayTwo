7f1bd57494e368e9f0ec360dbd8d361460f534f0
Kochin Wang <kcwang.work@gmail.com>
2016-07-12 22:36:55 +0800

    測試案例4：新增測試並執行顯示紅燈

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -64,5 +64,26 @@ namespace PotterShoppingCart.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_Buy_1_volumeOne_and_1_volumeTwo_and_1_volumeThree_and_1_volumeFour_price_should_be_320()
+        {
+            // arrange
+            var target = new ShoppingCart();
+            var order = new List<PotterSeries>
+            {
+                new PotterSeries { Volume = "One", Quantity = 1 },
+                new PotterSeries { Volume = "Two", Quantity = 1 },
+                new PotterSeries { Volume = "Three", Quantity = 1 },
+                new PotterSeries { Volume = "Four", Quantity = 1 }
+            };
+
+            // act
+            int actual = target.CheckOut(order);
+            var expected = 320;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
